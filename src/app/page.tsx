9cec5dce83cf08f36f291f298f0745a366fc0223
StevetheRebel <steve.mogan51@gmail.com>
2026-08-14 22:04:38 +0300

    refactor: simplify home page to just support dashboard wrapper - remove duplicate layout and navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <section className="max-w-350 mx-auto px-4 md:px-8 lg:px-16 py-8">
+    <section className="px-4 md:px-8 lg:px-16 py-8">
       
     </section>
   );
