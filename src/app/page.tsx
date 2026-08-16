f31117f98b10d07ce9c5f62103aea7dc92de150d
StevetheRebel <steve.mogan51@gmail.com>
2026-08-16 16:29:06 +0300

    feat: assemble home page with Hero, Shop, Stock, Contact, and Footer Sections - build full landing page with invetory data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { ContactSection } from "../components/home/ContactSection";
+import { Footer } from "../components/home/Footer";
+import HeroSection from "../components/home/HeroSection";
+import ShopSection from "../components/home/ShopSection";
+import StockDashboard from "../components/home/StockDashboard";
+
 export default function Home() {
   return (
     <section className="px-4 md:px-8 lg:px-16 py-8">
-      
+      <HeroSection />
+      <ShopSection />
+      <StockDashboard />
+      <ContactSection />
+      <Footer />
     </section>
   );
 }
